0151b80a3a8b705c27c44666eff3b024e276147a
mykolassl <emykolas@gmail.com>
2022-07-20 20:49:57 +0300

    Add prototype of adding a book to the page

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
 const addBookBtn = document.querySelector('#add-book');
 const bookDialog = document.querySelector('#book-dialog');
 const cancelFormBtn = document.querySelector('#cancel-form');
+const form = document.querySelector('form[method="dialog"]');
+
+let bookList = [];
+
+function Book(_author, _title, _pages, _hasBeenRead) {
+    this.author  = _author;
+    this.title = _title;
+    this.pages = _pages;
+    this.hasBeenRead = _hasBeenRead;
+    this.setReadStatus = function() {
+        this.hasBeenRead = !this.hasBeenRead;
+    }
+}
+
+function renderBooks() {
+    bookList.forEach((book) => {
+        const bookDiv = document.createElement('div');
+        bookDiv.classList.add('book');
+        bookDiv.innerHTML = `<h3>${book.title}</h3>
+        <p>${book.author}</p>
+        <p>${book.pages} pages</p>
+        <p>${book.hasBeenRead ? 'Read' : 'Not read'}</p>`;
+        document.querySelector('main').appendChild(bookDiv);
+    });
+}
 
 addBookBtn.addEventListener('click', () => {
     bookDialog.showModal();
@@ -8,4 +33,16 @@ addBookBtn.addEventListener('click', () => {
 
 cancelFormBtn.addEventListener('click', () => {
     bookDialog.close();
+});
+
+form.addEventListener('submit', () => {
+    const author = document.querySelector('#author').value;
+    const title = document.querySelector('#title').value;
+    const pages = document.querySelector('#pages').value;
+    const hasBeenRead = document.querySelector('#read-status').checked;
+
+    const book = new Book(author, title, pages, hasBeenRead);
+    bookList.push(book);
+
+    renderBooks();
 });
